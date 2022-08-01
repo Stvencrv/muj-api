ae8d67d1744edf8c6f901db7cd753357a0b7f2fe
Huascar Mejia <mejiahuascar@gmail.com>
2022-08-01 19:43:34 -0400

    fix: deleting some files

--- dist/routes/order.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const order_controller_1 = require("../controllers/order.controller");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post("/add", order_controller_1.createOrders);
-router.get('/', order_controller_1.getOrders);
-router.put("/:id/orderStatus", order_controller_1.updateOrderStatus);
-exports.default = router;
